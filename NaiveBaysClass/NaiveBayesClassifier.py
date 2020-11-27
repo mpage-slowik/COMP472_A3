05437d4d7625ffed9ffab30092b3d139f515f727
WilliamBergamin <berw@videotron.ca>
2020-11-27 10:56:21 -0500

    clean up

--- NaiveBaysClass/NaiveBayesClassifier.py
+++ NaiveBaysClass/NaiveBayesClassifier.py
@@ -8,31 +8,31 @@ SMOOTHING_ADDITIVE = 0.01
 
 class NaiveBayesClassifier:
 
-    def __init__(self, dataframe):
-        self.total_size = len(dataframe.index)
-        self.vocabulary_size = len(dataframe.columns)
-        self.log_prob_factual_claim = float()
-        self.log_prob_not_factual_claim = float()
-        self.log_probablility_table_factual_claim = 0
-        self.log_probability_table_not_factual_claim = 0
-        self.dataset_contain_factual_claim = dataframe[dataframe[FACTUAL_CLAIM_ID] == FACTUAL_CLAIM_VALUE]
-        self.dataset_not_contain_factual_claim = dataframe[dataframe[FACTUAL_CLAIM_ID] == NOT_FACTUAL_CLAIM_VALUE]
+    def __init__(self, dataset):
+        self.total_size = len(dataset.index)
+        self.vocabulary_size = len(dataset.columns)
+        self.log_probability_factual = float()
+        self.log_probability_not_factual = float()
+        self.log_probablility_table_factual = None
+        self.log_probability_table_not_factual = None
+        self.dataset_contain_factual_claim = dataset[dataset[FACTUAL_CLAIM_ID] == FACTUAL_CLAIM_VALUE]
+        self.dataset_contain_not_factual_claim = dataset[dataset[FACTUAL_CLAIM_ID] == NOT_FACTUAL_CLAIM_VALUE]
 
     def fit(self):
         self._set_pobability_of_factual_and_not()
         self._set_probablility_tables_for_factual_and_not()
 
     def _set_pobability_of_factual_and_not(self):
-        self.log_prob_factual_claim = math.log(len(
+        self.log_probability_factual = math.log(len(
             self.dataset_contain_factual_claim.index)/self.total_size)
-        self.log_prob_not_factual_claim = math.log(len(
-            self.dataset_not_contain_factual_claim.index)/self.total_size)
+        self.log_probability_not_factual = math.log(len(
+            self.dataset_contain_not_factual_claim.index)/self.total_size)
 
     def _set_probablility_tables_for_factual_and_not(self):
-        self.log_probablility_table_factual_claim = self._get_probablility_table(
+        self.log_probablility_table_factual = self._get_probablility_table(
             self.dataset_contain_factual_claim)
-        self.log_probability_table_not_factual_claim = self._get_probablility_table(
-            self.dataset_not_contain_factual_claim)
+        self.log_probability_table_not_factual = self._get_probablility_table(
+            self.dataset_contain_not_factual_claim)
 
     def _get_probablility_table(self, dataset):
         probability_table = dataset.drop(columns=[FACTUAL_CLAIM_ID]).sum(axis=0)
@@ -47,10 +47,10 @@ class NaiveBayesClassifier:
         return NOT_FACTUAL_CLAIM_VALUE, not_factual_probability
 
     def _get_factual_claim_probability(self, words):
-        return self.log_prob_factual_claim + self._get_attribute_probability(words, self.log_probablility_table_factual_claim)
+        return self.log_probability_factual + self._get_attribute_probability(words, self.log_probablility_table_factual)
 
     def _get_not_factual_claim_probability(self, words):
-        return self.log_prob_not_factual_claim + self._get_attribute_probability(words, self.log_probability_table_not_factual_claim)
+        return self.log_probability_not_factual + self._get_attribute_probability(words, self.log_probability_table_not_factual)
 
     def _get_attribute_probability(self, words, probablility_table):
         probability = 0
